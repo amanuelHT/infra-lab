36b030b47084c8505792892d1e6c1e5a18578cac
amanuelHT <aman3hda@gmail.com>
2025-09-19 04:03:55 +0200

    -

--- apache/index.php
+++ apache/index.php
@@ -1,4 +1,6 @@
 <?php
-  echo "Hei fra infra-lab portalen aman....aaa.hawey.. !<br>";
+  echo "<h1>Velkommen til infra-lab portalen!</h1>";
+  echo "<p><a href='/app'>➡️ Åpne Java-appen (Tomcat)</a></p>";
+  echo "<p><a href='/dbtest.php'>📄 Se pasientliste (PostgreSQL via PHP)</a></p>";
   phpinfo();
 ?>
